cccaa6110f47b53fa806ff0782246574b7df7faa
JuliaValerio <juliav286@gmail.com>
2019-05-23 14:53:29 -0300

    Carroussel da page home finalizado

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var slider = tns({
+  container: '.my-slider',
+  items: 1,
+  slideBy: 'page',
+  // autoplay: true,
+  arrowKeys: true,
+  // autoWidth: true,
+  controlsContainer: '.navegacao', 
+  
+});
+
+
+
